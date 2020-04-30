2f2c4c7fbcd4d8ad7cca34d881127e687e681f7f
mih777 <mih777777@yandex.ru>
2020-04-30 22:17:21 +0300

    Добавил AUTH , регистрация и аунтификация

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://mih777:mih777@ds149511.mlab.com:49511/graph777'
+  mongoURI: 'mongodb://mih777:mih777@ds149511.mlab.com:49511/graph777',
+  jwt: 'dev-jwt'
 }
